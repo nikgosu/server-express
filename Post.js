c6552b6bf91462dac7ed84e404825f7f9dad28fd
NIKGOSU\GosuJungler <gosujungler@gmail.com>
2022-05-06 23:45:00 +0300

    add env.PORT

--- Post.js
+++ Post.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose"
 
 const Post = new mongoose.Schema({
-	_id: {type: String, required: false},
 	info: {type: Object, required: true},
 	isImportant: {type: Boolean, required: true},
 	isDone: {type: Boolean, required: true}
