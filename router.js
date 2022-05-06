ae3af22d27b57b051b4d814e04b8e3fea1155825
NIKGOSU\GosuJungler <gosujungler@gmail.com>
2022-05-06 21:40:57 +0300

    finish fs server

--- router.js
+++ router.js
@@ -3,10 +3,10 @@ import PostController from "./PostController.js"
 
 const router = Router()
 
-router.post('posts', PostController.create)
-router.get('posts', PostController.getAll)
-router.get('posts/:id', PostController.getOne)
-router.put('posts', PostController.update)
-router.delete('posts/:id', PostController.delete)
+router.post('/posts', PostController.create)
+router.get('/posts', PostController.getAll)
+router.get('/posts/:id', PostController.getOne)
+router.put('/posts', PostController.update)
+router.delete('/posts/:id', PostController.delete)
 
 export default router
